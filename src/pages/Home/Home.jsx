a42526059eaf280db7c72256db56c943716ab548
Naresh Bhosale <nareshbhosale73@gmail.com>
2023-10-22 18:11:30 +0530

    first coomi

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Home = () => {
-  return <div style={{ height: "1222vh" }}>Hello Naresh</div>;
+  return <div style={{ height: "1222vh" }}>Hello Naresh 2</div>;
 };
 
 export default Home;
