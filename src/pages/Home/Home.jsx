d9bb36347510a860a516d452a97650ebd954fb54
Naresh Bhosale <nareshbhosale73@gmail.com>
2023-10-22 18:20:46 +0530

    a

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Home = () => {
-  return <div style={{ height: "1222vh" }}>Hello </div>;
+  return <div style={{ height: "1222vh" }}>Hello 1</div>;
 };
 
 export default Home;
