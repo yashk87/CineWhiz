480dd65c96a416f78ea351dc56f90c71ad506ccc
Naresh Bhosale <nareshbhosale73@gmail.com>
2023-10-24 00:03:35 +0530

    some-code-is-updated

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,7 @@ import AppAlert from "./utils/AppAlert/AppAlert";
 import AppLoader from "./utils/AppLoader/AppLoader";
 import TopLoadingBar from "./utils/TopLoadingBar/TopLoadingBar";
 import TopNav from "./utils/TopNav/TopNav";
+
 // mulitple screen size xs,sm,md,lg,xl
 
 function App() {
--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,8 +1,12 @@
 import { Card } from "@mui/material";
 import React from "react";
+import LoginForm from "./components/login-form";
 
 const Login = () => {
   return (
+    // <header className="bg-blue-500 text-white text-center p-4">
+    //   Welcome to React with Tailwind CSS!
+    // </header>
     <div
       style={{
         display: "flex",
@@ -13,8 +17,11 @@ const Login = () => {
         boxSizing: "border-box",
       }}
     >
-      <Card style={{ width: "95%", height: 450 }}>
-        "<div></div>
+      <Card style={{ width: "95%", height: 450 }} className="grid grid-cols-2">
+        <div className=" bg-blue-500"></div>
+        <div className="flex justify-center items-center bg-[#F8F8F8]">
+          <LoginForm />
+        </div>
       </Card>
     </div>
   );
