da8fdc30d291305dc6079642d50698d5da9116dd
dumbremegha <dumbremegha3232@gmail.com>
2023-10-30 12:04:29 +0530

    file save

--- src/Route.jsx
+++ src/Route.jsx
@@ -7,18 +7,18 @@ import Home from "./pages/Home/Home";
 import SignIn from "./pages/SignIn/SignIn";
 import Signup from "./pages/SignUp/Signup";
 import DateRangeCalendarValue from "./pages/Test/test2";
-
+import Emailverify from "./components/emailverify/emailverify";
 const App = () => {
   return (
     <Routes>
       <Route exact path="/" element={<Home />} />
       <Route exact path="/about" element={<About />} />
-      <Route exact path="/sign-in" element={<SignIn />} />
+      <Route exact path="/sign-in" element={<SignIn />} />s
       <Route exact path="/sign-up" element={<Signup />} />
       <Route exact path="/add-organisation" element={<AddOrganisation />} />
-
       <Route exact path="/addDepartment" element={<Department />} />
       <Route exact path="/menu" element={<DateRangeCalendarValue />} />
+      <Route exact path="/:id/verify/:token/" element={<Emailverify />} />
     </Routes>
   );
 };
