2639b0e0ad62be342d184ffaa002249d5fa752b8
Naresh Bhosale <nareshbhosale73@gmail.com>
2023-10-31 10:32:56 +0530

    some update

--- src/pages/Home/components/Organisation.jsx
+++ src/pages/Home/components/Organisation.jsx
@@ -1,7 +1,39 @@
+import {
+  Card,
+  CardActionArea,
+  CardContent,
+  CardMedia,
+  Typography,
+} from "@mui/material";
 import React from "react";
 
 const Organisation = () => {
-  return <div>Organisation</div>;
+  return (
+    <Card sx={{ maxWidth: 345 }}>
+      <CardActionArea>
+        <CardMedia
+          component="img"
+          className="h-[140px]"
+          image="https://mui.com/static/images/cards/contemplative-reptile.jpg"
+          alt="green iguana"
+        />
+        <CardContent>
+          <Typography
+            color={"#1D6EB7"}
+            gutterBottom
+            variant="h5"
+            component="div"
+          >
+            Organisation-1
+          </Typography>
+          <Typography variant="body2" color="text.secondary">
+            Lizards are a widespread group of squamate reptiles, with over 6,000
+            species, ranging across all continents except Antarctica
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+    </Card>
+  );
 };
 
 export default Organisation;
---
+++ src/pages/Home/components/cyclic-text.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import Typed from "react-typed";
+
+function TextCycler() {
+  const textArray = [
+    "Hi We Are ARGAN Tech!",
+    "Welcome to our AEIG'S Software",
+    "",
+  ];
+
+  return (
+    <div className="pb-4">
+      <Typed
+        strings={[
+          "Hi Welcome to <div class='inline text-red-500'> AEIGS</div> 👨‍💻 software !",
+          "Software build for <div class='inline text-red-500 pulse' >HR </div>🧑🏻‍💻 !",
+          "Software build for <div class='inline text-red-500 pulse' >companies like you </div> 🫵 !",
+          "Software build for<div class='inline text-red-500 pulse' > those who know <div class='inline underline font-extrabold'> Importance</div> of simplicity </div> 🙂 !",
+        ]}
+        typeSpeed={50}
+        backSpeed={30}
+        className="text-[#1976d2] font-bold"
+        loop
+      />
+    </div>
+  );
+}
+
+export default TextCycler;
