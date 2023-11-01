5b49dc7d5841fc9d4e5d64bb50f81b0360d5a72b
Naresh Bhosale <nareshbhosale73@gmail.com>
2023-11-01 13:01:03 +0530

    git push deleted the subfolder

--- frontend/src/pages/Test/test.jsx
+++
@@ -1,9 +0,0 @@
-import SwipeableTemporaryDrawer from "../../components/app-layout/components/swipable-drawer";
-
-export default function MiniDrawer() {
-  return (
-    <div>
-      <SwipeableTemporaryDrawer />
-    </div>
-  );
-}
