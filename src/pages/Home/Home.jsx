5871cff10b1312fb5e79f351f85679b189ea9e40
Naresh Bhosale <nareshbhosale73@gmail.com>
2023-11-02 12:57:00 +0530

    git-commit-my- last record

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,14 +1,47 @@
-import { Divider } from "@mui/material";
-import React from "react";
+import { ChevronRightOutlined } from "@mui/icons-material";
+import { Divider, Fab } from "@mui/material";
+import React, { useContext, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import { TestContext } from "../../State/Function/Main";
+import { UseContext } from "../../State/UseState/UseContext";
 import Organisation from "./components/Organisation";
 import TextCycler from "./components/cyclic-text";
 
 const Home = () => {
+  const { cookies } = useContext(UseContext);
+  const { handleAlert } = useContext(TestContext);
+  const redirect = useNavigate();
+  useEffect(() => {
+    const authToken = cookies["aeigs"];
+    if (!authToken) {
+      // Redirect to the login page
+      redirect("/sign-in");
+      handleAlert(true, "warning", "Please login first.");
+    }
+  }, [redirect, cookies, handleAlert]);
+  const handleScroll = (e) => {
+    console.log(e);
+    // window.scrollLeft = 10;
+    console.log("e", (e.target.parentElement.scrollLeft = -10));
+  };
+
   return (
     <div className="p-8">
       <TextCycler />
       <Divider />
-      <Organisation />
+      <div className="flex w-full p-4 gap-12 overflow-y-scroll overflow-visible relative">
+        <Organisation />
+        <Organisation />
+        <Organisation />
+        <Organisation />
+        <Organisation />
+        <Organisation />
+        <div className="absolute right-0 bottom-1/2">
+          <Fab aria-label="like" onClick={handleScroll}>
+            <ChevronRightOutlined />
+          </Fab>
+        </div>
+      </div>
     </div>
   );
 };
--- src/pages/Home/components/Organisation.jsx
+++ src/pages/Home/components/Organisation.jsx
@@ -6,33 +6,40 @@ import {
   Typography,
 } from "@mui/material";
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Organisation = () => {
   return (
-    <Card sx={{ maxWidth: 345 }}>
-      <CardActionArea>
-        <CardMedia
-          component="img"
-          className="h-[140px]"
-          image="https://mui.com/static/images/cards/contemplative-reptile.jpg"
-          alt="green iguana"
-        />
-        <CardContent>
-          <Typography
-            color={"#1D6EB7"}
-            gutterBottom
-            variant="h5"
-            component="div"
-          >
-            Organisation-1
-          </Typography>
-          <Typography variant="body2" color="text.secondary">
-            Lizards are a widespread group of squamate reptiles, with over 6,000
-            species, ranging across all continents except Antarctica
-          </Typography>
-        </CardContent>
-      </CardActionArea>
-    </Card>
+    <Link
+      to={"/add-organisation"}
+      className=" cursor-pointer hover:scale-95 scroll-smooth  transition-all"
+    >
+      <Card sx={{ width: "-webkit-fill-available", overflow: "visible" }}>
+        <CardActionArea disabled className="!w-[300px]">
+          <CardMedia
+            component="img"
+            className="h-[140px] rounded-t-md"
+            image="https://mui.com/static/images/cards/contemplative-reptile.jpg"
+            alt="green iguana"
+          />
+          <CardContent>
+            <Typography
+              color={"#1D6EB7"}
+              gutterBottom
+              variant="h5"
+              component="div"
+              className="hover:underline"
+            >
+              Organisation-1
+            </Typography>
+            <Typography variant="body2" color="text.secondary">
+              Lizards are a widespread group of squamate reptiles, with over
+              6,000 species, ranging across all continents except Antarctica
+            </Typography>
+          </CardContent>
+        </CardActionArea>
+      </Card>
+    </Link>
   );
 };
 
