b8a96ccbbdf4fb5a31a67db69ddcd6e112852b91
dev2 <dev2.atsit@gmail.com>
2023-11-11 11:18:10 +0530

    base changes

--- src/Route.jsx
+++ src/Route.jsx
@@ -2,29 +2,16 @@ import React from "react";
 import { Route, Routes } from "react-router-dom";
 import AnimationComponent from "./components/emailverify/verification-animation";
 import ForgotPassword from "./components/forgotpassword/forgotpassword";
-import Notification from "./components/profieicon/components/notification";
-import ResetPassword from "./components/resetpassword/resetpassword";
 import About from "./pages/About/About";
 import AddOrganisation from "./pages/AddOrganisation/AddOrganisation";
+import AddRole from "./pages/AddRole/AddRole";
 import Home from "./pages/Home/Home";
-import AddEmployee from "./pages/Home/components/addemployee";
-import SingleOrganization from "./pages/Organization/SingleOrganization";
+import ShiftsDisp from "./pages/SetupPage/ShiftDisplay";
+import Shifts from "./pages/SetupPage/Shifts";
 import SignIn from "./pages/SignIn/SignIn";
 import Signup from "./pages/SignUp/Signup";
 import DateRangeCalendarValue from "./pages/Test/test2";
-import WaitMain from "./pages/Waiting-comp/waiting-main";
 import Department from "./pages/addDepartment/addDepartment";
-<<<<<<< HEAD
-import ForgotPassword from "./components/forgotpassword/forgotpassword";
-import AddRole from "./pages/AddRole/AddRole";
-import Shifts from "./pages/SetupPage/Shifts";
-import ShiftsDisp from "./pages/SetupPage/ShiftDisplay";
-=======
-import AddRoles from "./pages/Roles/AddRoles";
-import Setup from "./pages/SetUpOrganization/Setup";
-import PublicHoliday from "./pages/SetUpOrganization/PublicHolidayPage/PublicHoliday";
-import LeaveRequisitiion from "./pages/LeaveRequisition/LeaveRequisition";
->>>>>>> 4152908710c052a198a2bc07ddca841ed0283cdd
 const App = () => {
   return (
     <Routes>
@@ -37,29 +24,9 @@ const App = () => {
       <Route exact path="/menu" element={<DateRangeCalendarValue />} />
       <Route exact path="/verify/:token/" element={<AnimationComponent />} />
       <Route exact path="/forgot-password" element={<ForgotPassword />} />
-<<<<<<< HEAD
       <Route exact path="/add-role" element={<AddRole />} />
       <Route exact path="/set-shifts" element={<Shifts />} />
       <Route exact path="/set-shiftsdisp" element={<ShiftsDisp />} />
-=======
-      <Route exact path="/waiting" element={<WaitMain />} />
-      <Route exact path="/reset-password/:token" element={<ResetPassword />} />
-      <Route exact path="/organisation/:id" element={<SingleOrganization />} />
-      <Route
-        exact
-        path="/organisation/:id/add-employee"
-        element={<AddEmployee />}
-      />
-      <Route exact path="/add-roles/:id" element={<AddRoles />} />
-      <Route exact path="/notification" element={<Notification />} />
-      <Route exact path="/setup/:id" element={<Setup />} />
-      <Route exact path="/leave/:id" element={<LeaveRequisitiion />} />
-      <Route
-        exact
-        path="/setup/:id/public-holidays"
-        element={<PublicHoliday />}
-      />
->>>>>>> 4152908710c052a198a2bc07ddca841ed0283cdd
     </Routes>
   );
 };
