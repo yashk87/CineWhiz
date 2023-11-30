bc2a78224c0d2e344ff908346195787106d6ea95
dev3 <dev3.atsit@gmail.com>
2023-11-30 11:53:54 +0530

    updatedCode

--- src/Route.jsx
+++ src/Route.jsx
@@ -4,7 +4,6 @@ import AnimationComponent from "./components/emailverify/verification-animation"
 import ForgotPassword from "./components/forgotpassword/forgotpassword";
 import ResetPassword from "./components/resetpassword/resetpassword";
 import About from "./pages/About/About";
-import AddEmployee from "./pages/AddEmployee/addemployee";
 import AddOrganisation from "./pages/AddOrganisation/AddOrganisation";
 import AddRole from "./pages/AddRole/AddRole";
 import Application from "./pages/Application/Application";
@@ -16,7 +15,6 @@ import Notification from "./pages/Notification/notification";
 import SingleOrganization from "./pages/Organization/SingleOrganization";
 import AddRoles from "./pages/Roles/AddRoles";
 import EmployementTypes from "./pages/SetUpOrganization/EmployementType/EmployementTypes";
-import LeaveTypes from "./pages/SetUpOrganization/LeaveComponents/LeaveTypes";
 import OrganizationLocation from "./pages/SetUpOrganization/OrganizationLocations/OrganizationLocations";
 import PublicHoliday from "./pages/SetUpOrganization/PublicHolidayPage/PublicHoliday";
 import SalaryInput from "./pages/SetUpOrganization/SaleryInput/SalaryInput";
@@ -29,7 +27,6 @@ import MyCalendar from "./pages/Test/test2";
 import UserProfile from "./pages/UserProfile/UserProfile";
 import WaitMain from "./pages/Waiting-comp/waiting-main";
 import Department from "./pages/addDepartment/addDepartment";
-import Designation from "./pages/Designation/Designation";
 import EmailSetting from "./pages/SetupPage/EmailSetting";
 import AddEmployee from "./pages/AddEmployee/addemployee";
 import LeaveTypes from "./pages/SetUpOrganization/LeaveComponents/LeaveTypes";
@@ -55,8 +52,7 @@ const App = () => {
       <Route exact path="/add-role" element={<AddRole />} />
       <Route exact path="/add-inputfield/:id" element={<Inputfield />} />
       <Route exact path="/set-shifts/:id" element={<Shifts />} />
-      <Route exact path="/set-designation/:id" element={<Designation />} />
-      <Route exact path="/set-shiftsdisp" element={<ShiftsDisp />} />
+      {/* <Route exact path="/set-shiftsdisp" element={<ShiftsDisp />} /> */}
       <Route exact path="/set-designation" element={<Designation />} />
       <Route exact path="/waiting" element={<WaitMain />} />
       <Route exact path="/reset-password/:token" element={<ResetPassword />} />
