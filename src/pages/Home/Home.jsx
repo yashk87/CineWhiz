c1bd3da0bacc15a62470d8c0375e785dacc50510
Dev4 <dev4.atsit@gmail.com>
2023-12-05 11:52:16 +0530

    Welcome page updated

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,25 +1,32 @@
-import EastIcon from "@mui/icons-material/East";
-import WestIcon from "@mui/icons-material/West";
-import { Typography } from "@mui/material";
+import { Skeleton } from "@mui/material";
 import axios from "axios";
 import { jwtDecode } from "jwt-decode";
 import React, { useContext, useEffect, useState } from "react";
-import Carousel from "react-multi-carousel";
+import { FaArrowCircleRight } from "react-icons/fa";
 import "react-multi-carousel/lib/styles.css";
-import { useNavigate } from "react-router-dom";
+import { useQuery } from "react-query";
+import { Link, useNavigate } from "react-router-dom";
 import { TestContext } from "../../State/Function/Main";
 import { UseContext } from "../../State/UseState/UseContext";
-import Organisation from "./components/Organisation";
-
 const Home = () => {
-  const { cookies } = useContext(UseContext);
-  const { handleAlert } = useContext(TestContext);
   const redirect = useNavigate();
-  const [organizationData, setOrganizationData] = useState([]);
-  const [userRole, setUserRole] = useState();
-
+  const { cookies } = useContext(UseContext);
   const authToken = cookies["aeigs"];
+  const [userRole, setUserRole] = useState();
   const token = cookies["aeigs"];
+  const { handleAlert } = useContext(TestContext);
+
+  const { data, isLoading } = useQuery(["orgData"], async () => {
+    const response = await axios.get(
+      `${process.env.REACT_APP_API}/route/organization/get`,
+      {
+        headers: {
+          Authorization: authToken,
+        },
+      }
+    );
+    return response.data;
+  });
 
   useEffect(() => {
     try {
@@ -45,120 +52,65 @@ const Home = () => {
     }
   }, [redirect, cookies, handleAlert, authToken]);
 
-  const getData = async () => {
-    const data = await axios.get(
-      `${process.env.REACT_APP_API}/route/organization/get`,
-      {
-        headers: {
-          Authorization: authToken,
-        },
-      }
-    );
-
-    setOrganizationData(data.data.organizations);
-  };
-
-  useEffect(() => {
-    getData();
-    // eslint-disable-next-line
-  }, []);
-
-  const dotsresponsive = {
-    desktop: {
-      breakpoint: { max: 3000, min: 1400 },
-      items: 4,
-      slidesToSlide: 1,
-    },
-    tablet: {
-      breakpoint: { max: 1400, min: 1050 },
-      items: 3,
-      slidesToSlide: 1,
-    },
-    mobile: {
-      breakpoint: { max: 1050, min: 500 },
-      items: 2,
-      slidesToSlide: 1,
-    },
-  };
-
-  const CustomRightArrow = ({ onClick }) => {
-    // onMove means if dragging or swiping in progress.
-    return (
-      <button
-        className="p-2 rounded-full border-sky-600 font-bold bg-slate-300 text-sky-600 border absolute right-0"
-        onClick={() => onClick()}
-      >
-        <EastIcon />
-      </button>
-    );
-  };
-
-  const CustomLeftArrow = ({ onClick }) => {
-    return (
-      <button
-        className="p-2 rounded-full text-[2px] border-sky-600 font-bold bg-slate-300 text-sky-600 border absolute left-0"
-        onClick={() => onClick()}
-      >
-        <WestIcon className="h-2 w-4 text-[5px]" />
-      </button>
-    );
-  };
-
   return (
     <div className="p-8 bg-white h-screen">
       {/* <TextCycler />
       <Divider /> */}
       {/* <Org /> */}
       <div className="w-full  ">
-        {organizationData.length >= 0 ? (
-          <>
-            <div className="flex items-center h-[60vh] justify-center w-full">
-              <div className="w-[50%] px-8   flex justify-start items-end  flex-col">
-                <div>
-                  <h1 className="text-5xl font-semibold text-blue-500 ">
-                    Welcome to AGIES {userRole?.first_name}{" "}
-                    {userRole?.last_name}
-                  </h1>
-                  <h2 className="text-2xl mb-4 font-semibold">
-                    Unleashing Organizational Excellence
-                  </h2>
+        <>
+          <div className="flex items-center h-[70vh] justify-center w-full">
+            <div className="md:w-[50%] w-[100%]  md:px-8  xs:px-2 flex justify-end items-end  flex-col">
+              <div>
+                <h1 className="md:text-[2.30rem] xs:text-[1.5rem] font-thin">
+                  Welcome to{" "}
+                  <span className="md:text-[2.30rem] xs:text-[1.5rem]  gradinet font-semibold text-blue-500 ">
+                    AGIES
+                  </span>{" "}
+                  <span className="font-bold">{userRole?.first_name}</span>
+                </h1>
+                <h1 className="md:text-[2.40rem]  xs:text-[1.40rem] sm:text-[1.70rem] !leading-10 sm:text-2xl font-bold  mb-4">
+                  Unleashing
+                  <span className="gradinet font-bold">
+                    {" "}
+                    Organizational Excellence
+                  </span>
+                </h1>
 
-                  <button className=" flex justify-center rounded-md px-3 py-2 text-sm font-semibold text-white bg-blue-500 hover:bg-blue-500 focus-visible:outline-blue-500">
-                    Create your organization
-                  </button>
-                </div>
-              </div>
+                <p className="md:text-xl xs:text-md mb-8 text-gray-600 md:leading-10 xs:leading-5 ">
+                  Empower your journey by making us your first choice. Elevate
+                  your experience with the{" "}
+                  <span className="!text-bold gradinet">AGIES</span>, Lets start
+                </p>
 
-              <div className="w-[50%]">
-                <img
-                  src="community.svg"
-                  className=" px-8 h-[40vh]"
-                  alt="none"
-                />
+                {isLoading ? (
+                  <Skeleton
+                    variant="rounded"
+                    height={50}
+                    className="w-[35%]"
+                    animation="pulse"
+                  />
+                ) : data?.organizations.length <= 0 ? (
+                  <Link to={"/add-organisation"}>
+                    <button className=" flex justify-center  gap-2 items-center rounded-md px-4 py-3 text-md font-semibold text-white bg-blue-500 hover:bg-blue-500 focus-visible:outline-blue-500">
+                      Create your organization <FaArrowCircleRight />
+                    </button>
+                  </Link>
+                ) : (
+                  <Link to={"/organizationList"}>
+                    <button className=" flex justify-center gap-2 items-center rounded-md px-4 py-3 text-md font-semibold text-white bg-blue-500 hover:bg-blue-500 focus-visible:outline-blue-500">
+                      Go to Organization <FaArrowCircleRight />
+                    </button>
+                  </Link>
+                )}
               </div>
             </div>
-          </>
-        ) : (
-          <>
-            <Typography variant="body1" className=" !font-medium !text-2xl">
-              List of Organization
-            </Typography>
 
-            <Carousel
-              swipeable={false}
-              draggable={false}
-              customRightArrow={<CustomRightArrow />}
-              customLeftArrow={<CustomLeftArrow />}
-              responsive={dotsresponsive}
-            >
-              {organizationData.map((item, index) => (
-                <div className="h-max py-4" key={index}>
-                  <Organisation item={item} />
-                </div>
-              ))}
-            </Carousel>
-          </>
-        )}
+            <div className="w-[50%] md:block hidden">
+              <img src="community.gif" className="h-[100%]" alt="none" />
+            </div>
+          </div>
+        </>
       </div>
 
       {/* <Org /> */}
