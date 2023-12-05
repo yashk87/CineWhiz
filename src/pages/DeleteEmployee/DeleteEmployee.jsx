10d2b63165f2b18d8f1006d011e31e8fc6321527
dumbremegha <dumbremegha3232@gmail.com>
2023-12-05 11:53:44 +0530

    file saved

--- src/pages/DeleteEmployee/DeleteEmployee.jsx
+++ src/pages/DeleteEmployee/DeleteEmployee.jsx
@@ -212,7 +212,7 @@ const DeleteEmployee = () => {
       setConfirmDeleteDialog(true);
     }
   };
-  console.log(excelDataToDelete);
+
   const handleConfirmDeleteExcelData = async () => {
     try {
       // Assuming each item in `excelDataToDelete` contains an `_id` field
